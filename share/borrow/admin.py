e49c133580a3a184c4d1035f34d968ed61b25614
jimwu0516 <125996962+jimwu0516@users.noreply.github.com>
2024-04-23 00:02:04 +0800

    update Remaining Time on user_orders page

--- share/borrow/admin.py
+++ share/borrow/admin.py
@@ -4,5 +4,5 @@ from .models import Order
 # Register your models here.
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_id', 'borrower', 'item', 'start_time','end_time','status','request_time','breakage')
-    fields = ('borrower', 'item', 'start_time','end_time','status','breakage')
+    fields = ('borrower', 'item', 'start_time','end_time','status','request_time','breakage')
 admin.site.register(Order, OrderAdmin)
