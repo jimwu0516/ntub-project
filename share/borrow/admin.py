d417f27912b9cca0000e4fa47590c105c38de303
jimwu0516 <125996962+jimwu0516@users.noreply.github.com>
2024-05-25 15:04:13 +0800

    add list_filter to borrow.admin.py

--- share/borrow/admin.py
+++ share/borrow/admin.py
@@ -5,4 +5,5 @@ from .models import Order
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('order_id', 'borrower', 'item', 'start_time','end_time','status','request_time','breakage','airdropAmount')
     fields = ('borrower', 'item', 'start_time','end_time','status','request_time','breakage')
+    list_filter = ('status', 'borrower', 'item', 'start_time', 'end_time') 
 admin.site.register(Order, OrderAdmin)
