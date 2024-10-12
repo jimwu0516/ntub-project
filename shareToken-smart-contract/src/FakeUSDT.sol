fa27644984029d7ee9427e92fd5c31fa79f82f10
jimwu0516 <125996962+jimwu0516@users.noreply.github.com>
2024-10-12 15:14:05 +0800

    add FakeUSDT

---
+++ shareToken-smart-contract/src/FakeUSDT.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract FakeUSDT {
+    string public name = "FakeUSDT";
+    string public symbol = "USDT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 10000000 * 10**uint256(decimals);
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function mint(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Invalid address");
+
+        totalSupply += _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(address(0), _to, _value);
+        return true;
+    }
+
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        totalSupply -= _value;
+        balanceOf[msg.sender] -= _value;
+
+        emit Transfer(msg.sender, address(0), _value);
+        return true;
+    }
+}
